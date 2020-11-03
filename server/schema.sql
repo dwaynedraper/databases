1e6a95e302e05cc614ce323e23a4f5131d5878aa
Dwayne <deandraper82@gmail.com>
2020-11-02 22:53:20 -0600

    Pushing for the night

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,26 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
+  id int NOT NULL auto_increment,
+  message_body varchar(500),
+  dateCreated date,
+  roomname char,
+  username char,
   /* Describe your table here.*/
+  PRIMARY KEY (id),
+
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  roomname char
+);
 
-
+ALTER TABLE messages ADD FOREIGN KEY (roomname) REFERENCES rooms(roomname);
 
 
 /*  Execute this file from the command line by typing:
