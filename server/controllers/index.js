3cf4569caea0022bea8fbdffbd2a4852d6701b92
Dwayne <deandraper82@gmail.com>
2020-11-05 07:09:12 -0600

    Pushing for the day

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,26 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+
+      let modelsGet = () => {
+        let promise = new Promise((resolve, reject) => {
+          resolve(models.messages.get());
+        });
+
+        return promise;
+      };
+
+      modelsGet()
+        .then((data) => {
+          //somehow add data to response callback
+          //likely have to reformat to JSON or something the client expects
+          console.log(data);
+          // res.send(data);
+        });
+
+
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
