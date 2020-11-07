13540c33f7c321423b40628fce24d9a7c4985801
Chris Tulin <47799223+christulin@users.noreply.github.com>
2020-11-07 13:26:48 -0800

    Complete refactoring with Sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,16 @@
 var db = require('../db').dbConnection;
+var Sequelize = require('sequelize');
+var sdb = new Sequelize('chat', 'root', '1117');
+
+var message = sdb.define('message', {
+  messagebody: Sequelize.STRING,
+  roomname: Sequelize.STRING,
+  username: Sequelize.STRING,
+  dateCreated: Sequelize.DATE,
+
+}, {
+  timestamps: false
+});
 
 module.exports = {
   messages: {
@@ -26,8 +38,11 @@ module.exports = {
     // a function which produces all the messages
     post: function (messagebody, roomname, username) {
 
-      db.query(`INSERT INTO messages (messagebody, roomname, username) VALUES ('${messagebody}', '${roomname}', '${username}');`);
-
+      //db.query(`INSERT INTO messages (messagebody, roomname, username) VALUES ('${messagebody}', '${roomname}', '${username}');`);
+      message.sync()
+        .then(function() {
+          message.create({messagebody, roomname, username})
+        });
     } // a function which can be used to insert a message into the database
   },
 
